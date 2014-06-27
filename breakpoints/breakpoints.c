9b2ff7c4d46b9d5447123c3d089f7b30850952b5
Michael Castanieto <mcastanieto@gmail.com>
2014-06-26 20:03:26 -0700

    added val_at_brktime function to the breakpoints library

--- breakpoints/breakpoints.c
+++ breakpoints/breakpoints.c
@@ -102,3 +102,36 @@ int inrange(const BREAKPOINT* points, double minval, double maxval, unsigned lon
 	}	
 	return range_OK;
 }
+
+/* find the value for a given time in a breakpoint array */
+double val_at_brktime(const BREAKPOINT* points, unsigned long npoints, double time)
+{
+	unsigned long i;
+	BREAKPOINT left, right;
+	double frac, val, width;
+
+	/* scan until we find a span containing our time */
+	for (i=1; i < npoints; i++)
+	{
+		if (time <= points[i].time)
+			break;
+	}
+
+	/* maintain final value if time is beyond end of data */
+	if (i==npoints)
+		return points[i-1].value;
+
+	left = points[i-1];
+	right = points[i];	
+
+	/* check for instant jump - two points with the same time */
+	width = right.time - left.time;
+	if (width==0.0)
+		return right.value;
+
+	/* get value from this span using linear interpolation */
+	frac = (time - left.time) / width;
+	val = left.value + (( right.value - left.value) * frac);
+
+	return val;
+}	
--- breakpoints/include/breakpoints.h
+++ breakpoints/include/breakpoints.h
@@ -21,3 +21,6 @@ BREAKPOINT maxpoint(const BREAKPOINT *points, long npoints);
 /* scan an array of breakpoint values and check if they are within the inputed range
    return 1 if in range, return 0 if out of range */
 int inrange(const BREAKPOINT* points, double minval, double maxval, unsigned long npoints); 
+
+/* find a value for a given time using an array of breakpoints */
+double val_at_brktime(const BREAKPOINT* points, unsigned long npoints, double time);
