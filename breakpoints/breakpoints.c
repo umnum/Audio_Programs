948ed9790ab7699f905db7461ab361835d69b295
Michael Castanieto <mcastanieto@gmail.com>
2014-06-27 17:25:29 -0700

    added minpoint to the breakpoints library

--- breakpoints/breakpoints.c
+++ breakpoints/breakpoints.c
@@ -86,6 +86,24 @@ BREAKPOINT maxpoint(const BREAKPOINT *points, long npoints)
 	return point;
 }
 
+/* return the breakpoint with the smallest value */
+BREAKPOINT minpoint(const BREAKPOINT* points, long npoints)
+{
+	int i;
+	BREAKPOINT point;
+	point.time = points[0].time;
+	point.value = points[0].value;
+	for (i = 0; i < npoints; i++)
+	{
+		if (point.value > points[i].value)
+		{
+			point.value = points[i].value;
+			point.time = points[i].time;
+		}
+	}
+	return point;
+}
+
 /* scan breakpoints and check if they are within the inputed range */
 int inrange(const BREAKPOINT* points, double minval, double maxval, unsigned long npoints)
 {
--- breakpoints/include/breakpoints.h
+++ breakpoints/include/breakpoints.h
@@ -18,6 +18,9 @@ BREAKPOINT *get_breakpoints(FILE *fp, long *psize);
 /* return the breakpoint with the largest value */
 BREAKPOINT maxpoint(const BREAKPOINT *points, long npoints);
 
+/* return the breakpoint with the smallest value */
+BREAKPOINT minpoint(const BREAKPOINT* points, long npoints);
+
 /* scan an array of breakpoint values and check if they are within the inputed range
    return 1 if in range, return 0 if out of range */
 int inrange(const BREAKPOINT* points, double minval, double maxval, unsigned long npoints); 
