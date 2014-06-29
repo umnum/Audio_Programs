84eb2e6ba9cb4006ffabd0feae7bda5ffa53d12e
Michael Castanieto <mcastanieto@gmail.com>
2014-06-28 17:51:28 -0700

    envx.c: extracts an amplitude envelope from a mono soundfile

--- CH2/envx/envx.c
+++ CH2/envx/envx.c
@@ -1,32 +1,69 @@
 /* envx.c: extract amplitude envelope from mono soundfile */ 
-/* USAGE: envx infile outfile */ 
+/* USAGE: envx [-wN] infile outfile */ 
 #include <stdio.h>
 #include <stdlib.h>
 #include <portsf.h>
 #include <breakpoints.h>
 #include <math.h>
-#define NFRAMES 100
+#define DEFAULT_WINDOW_MSECS 15
 
 enum {ARG_PROGNAME, ARG_INFILE, ARG_OUTFILE, ARG_NARGS};
 
 int main(int argc, char**argv)
 {
-	PSF_PROPS props;
-	long framesread, totalread; 
-	int size; 
+	PSF_PROPS inprops;
+	long framesread, /* number of frames copied to buffer */ 
+	     totalread; /* running count of sample frames */
+	unsigned long npoints; /* number of breakpoints generated */
+	unsigned long winsize;
+	char flag;
+	double brktime; /* holds the time for the current breakpoint time */
+	double windur = DEFAULT_WINDOW_MSECS;
 
 	/* init all resource vals to default states */ 
 	int ifd=-1; 
 	int error=0;
-	float* buffer = NULL;
+	float* inbuffer = NULL;
 	FILE* fp = NULL;
 
 	printf ("ENVX: extract amplitude envelope from mono soundfile.\n");
 
+	/* check for any options in command-line arguments */
+	if (argc>1)
+	{
+		while (argv[1][0]=='-')
+		{
+			flag = argv[1][1];
+			switch(flag)
+			{
+			/* TODO handle any flag arguments here */
+				case('\0'):
+					printf("ERROR: missing flag name.\n");
+					return 1;
+				case('w'):
+					windur = atof(&argv[1][2]);
+					if (windur <= 0.0)
+					{
+						printf("ERROR: bad value for window duration\n"
+						       "       must be positive\n");
+						return 1;	
+					}
+					break;
+				default:
+					break;
+			}
+			argc--;
+			argv++;
+		}
+	}
+
 	if (argc!=ARG_NARGS)
 	{
-		printf("insufficient arguments.\n"
-					 "USAGE:\tenvx infile outfile\n");
+		printf("ERROR:\tinsufficient arguments.\n"
+					 "USAGE:\tenvx [-wN] insndfile outfile.brk\n"
+		       "-wN: set extraction window size to N msecs.\n"
+		       "(default: 15)\n"
+		      );
 		return 1;
 	}
 
@@ -39,7 +76,7 @@ int main(int argc, char**argv)
 
 
 	/* open infile */ 
-	ifd = psf_sndOpen(argv[ARG_INFILE], &props, 0);
+	ifd = psf_sndOpen(argv[ARG_INFILE], &inprops, 0);
 	if (ifd<0)
 	{
 		printf("ERROR: unable to open infile \"%s\"\n",argv[ARG_INFILE]);
@@ -49,17 +86,8 @@ int main(int argc, char**argv)
 	/* we now have a resource, so we use goto hereafter
 		 on hitting any error */
 
-	/* get number of frames from infile */
-	size = psf_sndSize(ifd);
-	if(size<0)
-	{
-		printf("ERROR: unable to obtain the size of \"%s\"\n",argv[ARG_INFILE]);
-		error++;
-		goto exit;
-	}
-	
 	/* check if infile uses 8-bit samples*/ 
-	if (props.samptype==PSF_SAMP_8)
+	if (inprops.samptype==PSF_SAMP_8)
 	{
 		printf("ERROR: envx does not support 8-bit format.\n");
 		error++;
@@ -67,32 +95,36 @@ int main(int argc, char**argv)
 	}
 
 	/* check if infile is mono */
-	if (props.chans!=1)
+	if (inprops.chans!=1)
 	{
-		printf("ERROR: infile must be mono.\n");
+		printf("ERROR: infile has %d channels, must be mono.\n",inprops.chans);
 		error++;
 		goto exit;
 	}
 
 	/* display infile properties */
-	if(!psf_sndInfileProperties(argv[ARG_INFILE],ifd,&props))
+	if(!psf_sndInfileProperties(argv[ARG_INFILE],ifd,&inprops))
 	{
 		error++;
 		goto exit;
 	}	
 
-	/* create outfile */
+	/* create output breakpoint file */
 	fp = fopen(argv[ARG_OUTFILE],"w");
 	if (fp==NULL)
 	{
-		printf("ERROR: cannot open outfile: %s\n",argv[ARG_OUTFILE]);
+		printf("ERROR: unable to create breakpoint file: %s\n",argv[ARG_OUTFILE]);
 		error++;
 		goto exit;
 	}
 
+	/* set buffersize to the required envelope window size */
+	windur /= 1000.0; /* convert to secs */
+	winsize = (unsigned long)(windur * inprops.srate); /* number of frames */
+
 	/* allocate memory for infile buffer */
-	buffer= (float*)malloc(props.chans*sizeof(float)*NFRAMES);
-	if (buffer==NULL)
+	inbuffer= (float*)malloc(winsize * sizeof(float));
+	if (inbuffer==NULL)
 	{
 		puts("No memory!\n");
 		error++;
@@ -101,45 +133,57 @@ int main(int argc, char**argv)
 
 	printf("creating breakpoint file...\n");
 
-	int update=0; 
-
-	/* running count of sample frames */
+	/* initialize running count of sample frames */
 	totalread = 0; 
-		
-	/* loop every time NFRAMES are copied, report any errors */
-	while ((framesread = psf_sndReadFloatFrames(ifd,buffer,NFRAMES))>0)
+	/* initialize breakpoint time */
+	brktime = 0.0;	
+	/* initialize count of breakpoints */
+	npoints = 0;
+
+	/* loop every time winsize number of frames are copied, report any errors */
+	while ((framesread = psf_sndReadFloatFrames(ifd,inbuffer,winsize))>0)
 	{
-		update++;
-		
-		/* update infile reading status after refreshing the buffer every 100 times */
-		if (update%100==0)
-			printf("\r%ld samples read...  %ld%\r",totalread,100*totalread/size);
 
 		totalread+=framesread;
 	
-		//TODO <<-- Process sound file and breakpoint file here -->>
+		/* extract breakpoint value */
+		double amp;
+		amp = maxsamp(inbuffer,framesread);
+		
+		/* write breakpoint time and value to outfile */
+		if (fprintf(fp,"%f\t%f\n",brktime,amp)<2)
+		{
+			printf("Failed to write to breakpoint file: %s\n", argv[ARG_OUTFILE]);
+			error++;
+			goto exit;
+		}
 
+		npoints++;
+
+		brktime += windur;
 	}
 
 	if(framesread<0)
 	{
-		printf("Error reading infile. Outfile is incomplete.\n");
+		printf("\nError reading infile. Outfile is incomplete.\n");
 		error++;
 	}
 	else
-		printf("Done.\n"
-		       "sample frames read: %ld\n"
-		       "breakpoint file created: %s\n",
-						totalread, argv[ARG_OUTFILE]);
+		printf("\nDone: %d errors\n"
+		       "breakpoint file created: %s\n"
+		       "breakpoints written: %lu\n",
+						error, argv[ARG_OUTFILE], npoints
+		      );
 
 	/* do all the cleanup */
 	exit:
 	if (ifd>=0)
 		psf_sndClose(ifd);
 	if (fp)
-		fclose(fp);
-	if (buffer)
-		free(buffer);
+		if(fclose(fp))
+			printf("envx: failed to close breakpoint file: %s\n",argv[ARG_OUTFILE]);
+	if (inbuffer)
+		free(inbuffer);
 	psf_finish();
 	 	
 	return error;
