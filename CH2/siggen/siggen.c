c6b9d51f65067373fa5ce33451dc4242ac4f256d
Michael Castanieto <mcastanieto@gmail.com>
2014-07-03 16:57:53 -0700

    siggen: added sawtooth, triangle, and square waveform funtionality

--- CH2/siggen/siggen.c
+++ CH2/siggen/siggen.c
@@ -1,13 +1,17 @@
-/* signal generator for a simple sine wave */
-/* usage: siggen outsndfile duration srate amp freq */ 
+/* signal generator for a simple waveform */
+/* usage: siggen outsndfile wavetype duration srate nchans amp freq */ 
 #include <stdio.h>
 #include <stdlib.h>
 #include <portsf.h>
 #include <wave.h>
 #define NFRAMES 100 // size of buffer
 
-enum {ARG_PROGNAME, ARG_OUTFILE, ARG_DUR, ARG_SRATE,
+enum {ARG_PROGNAME, ARG_OUTFILE, ARG_TYPE,
+      ARG_DUR, ARG_SRATE, ARG_NCHANS, 
       ARG_AMP, ARG_FREQ, ARG_NARGS};
+
+enum {WAVE_SINE, WAVE_TRIANGLE, WAVE_SQUARE,
+      WAVE_SAWUP, WAVE_SAWDOWN, WAVE_NTYPES};
 
 int main (int argc, char**argv)
 {
@@ -16,6 +20,9 @@ int main (int argc, char**argv)
 	double amp, freq, dur, peakdiff;
 	unsigned long nbufs, outframes, remainder, nframes;
 	psf_format outformat = PSF_FMT_UNKNOWN;
+	int wavetype;
+	int nchans;
+	double (*tick)(OSCIL*,double);
 
 	/* init resource values */
 	int ofd=-1;
@@ -28,12 +35,17 @@ int main (int argc, char**argv)
 	if (argc!=ARG_NARGS)
 	{
 		printf("ERROR: insufficient number of arguments.\n"
-		       "USAGE: siggen outsndfile duration srate amp freq\n"
+		       "USAGE: siggen outsndfile wavetype duration srate nchans amp freq\n"
+		       "       wavetype:  sine, triangle, square, sawtooth_up, sawtooth_down\n" 
+		       "       duration:  duration of outfile (seconds)\n"
+		       "       srate:     required sample rate of outfile\n"
+		       "       amp:       amplitude (0 < amp <= 1.0)\n"
+		       "       freq:      frequency (freq > 0 )\n"
 		      );
 		return 1;
 	}
 
-	/* define outfile format - this sets 16-bit mono format */	
+	/* define outfile format - this sets 16-bit format */	
 	srate = atof(argv[ARG_SRATE]);		
 	if (srate<=0)
 	{
@@ -41,7 +53,6 @@ int main (int argc, char**argv)
 		return 1;
 	}
 	outprops.srate = srate;
-	outprops.chans = 1;
 	outprops.samptype = PSF_SAMP_16;
 	outprops.chformat = PSF_STDWAVE;
 	/* get outfile extension */
@@ -56,6 +67,87 @@ int main (int argc, char**argv)
 		goto exit;
 	}
 	outprops.format = outformat;
+	/* get number of channels */
+	nchans = atoi(argv[ARG_NCHANS]);
+	if (nchans > MC_WAVE_EX)
+	{
+		printf("ERROR: portsf does not support %d channels\n",
+		        nchans); 
+		return 1;
+	}
+	outprops.chans = nchans;	
+
+	/* get the wavetype */
+	int count = 0;
+	while (argv[ARG_TYPE][count]!='\0') count++;
+	switch (count)
+	{
+		case(4):
+			if ( (argv[ARG_TYPE][0]=='s')&&(argv[ARG_TYPE][1]=='i'&&
+					 (argv[ARG_TYPE][2]=='n')&& argv[ARG_TYPE][3]=='e')  ) 
+				wavetype = WAVE_SINE;
+			break;
+		case(6):
+			if ( (argv[ARG_TYPE][0]=='s')&&(argv[ARG_TYPE][1]=='q')&&
+			     (argv[ARG_TYPE][2]=='u')&&(argv[ARG_TYPE][3]=='a')&&
+			     (argv[ARG_TYPE][4]=='r')&&(argv[ARG_TYPE][5]=='e')   )
+				wavetype = WAVE_SQUARE;
+			break;
+		case(8):
+			if ( (argv[ARG_TYPE][0]=='t')&&(argv[ARG_TYPE][1]=='r')&&
+			     (argv[ARG_TYPE][2]=='i')&&(argv[ARG_TYPE][3]=='a')&&
+			     (argv[ARG_TYPE][4]=='n')&&(argv[ARG_TYPE][5]=='g')&&
+			     (argv[ARG_TYPE][6]=='l')&&(argv[ARG_TYPE][7]=='e')   )
+				wavetype = WAVE_TRIANGLE;
+			break;
+		case(11):
+			if ( (argv[ARG_TYPE][0]=='s')&&(argv[ARG_TYPE][1]=='a')&&
+			     (argv[ARG_TYPE][2]=='w')&&(argv[ARG_TYPE][3]=='t')&&
+			     (argv[ARG_TYPE][4]=='o')&&(argv[ARG_TYPE][5]=='o')&&
+			     (argv[ARG_TYPE][6]=='t')&&(argv[ARG_TYPE][7]=='h')&&
+			     (argv[ARG_TYPE][8]=='_')&&(argv[ARG_TYPE][9]=='u')&&
+			     (argv[ARG_TYPE][10]=='p')                            )
+				wavetype = WAVE_SAWUP;
+			break;
+		case(13):
+			if ( (argv[ARG_TYPE][0]=='s')&&(argv[ARG_TYPE][1]=='a')&&
+			     (argv[ARG_TYPE][2]=='w')&&(argv[ARG_TYPE][3]=='t')&&
+			     (argv[ARG_TYPE][4]=='o')&&(argv[ARG_TYPE][5]=='o')&&
+			     (argv[ARG_TYPE][6]=='t')&&(argv[ARG_TYPE][7]=='h')&&
+			     (argv[ARG_TYPE][8]=='_')&&(argv[ARG_TYPE][9]=='d')&&
+			     (argv[ARG_TYPE][10]=='o')&&(argv[ARG_TYPE][11]=='w')&&
+			     (argv[ARG_TYPE][12]=='n')                              )
+				wavetype = WAVE_SAWDOWN;
+			break;	
+		default:
+			wavetype = -1;		
+	}
+	if (wavetype<1)
+	{
+		printf("ERROR: you have chosen an unknown wavetype.\n"
+		       "wavetypes: sine, square, triangle, sawtooth_up, sawtooth_down\n"
+		      );
+		return 1;
+	}
+	
+	/* select waveform function */
+	switch (wavetype)
+	{
+		case(WAVE_SINE):
+			tick = sinetick;
+			break;
+		case(WAVE_TRIANGLE):
+			tick = tritick;
+			break;
+		case(WAVE_SQUARE):
+			tick = sqtick;
+			break;
+		case(WAVE_SAWUP):
+			tick = sawutick;
+			break;
+		case(WAVE_SAWDOWN):
+			tick = sawdtick;
+	}
 
 	/* get the time duration of the soundfile */
 	dur = atof(argv[ARG_DUR]);
@@ -102,7 +194,7 @@ int main (int argc, char**argv)
 
 	/* allocate a buffer for outfile */
 	nframes = NFRAMES;
-	outbuf = (float*)malloc(sizeof(float)*nframes);
+	outbuf = (float*)malloc(sizeof(float)*nframes*outprops.chans);
 
 	/* calculate the number of buffers */
 	nbufs = outframes/nframes;	
@@ -112,14 +204,16 @@ int main (int argc, char**argv)
 
 	printf("Creating soundfile...\n");
 
+	
 	/* process soundfile */	
-	long i,j;
+	long i,j,i_out;
 	for (i=0; i < nbufs; i++)
 	{ 
 		if ((i == (nbufs-1)) && remainder)
 			nframes = remainder;
 		for (j=0; j < nframes; j++)
-			outbuf[j] = (float)(amp * sinetick(p_osc,freq)); 
+			for (i_out=0; i_out < outprops.chans; i_out++)
+				outbuf[j*outprops.chans+i_out] = (float)(amp * tick(p_osc,freq)); 
 		if (psf_sndWriteFloatFrames(ofd,outbuf,nframes)!=nframes)
 		{
 			printf("Error writing to outfile\n");
